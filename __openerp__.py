0e753eedab95c38a7dd80885996436b6ae652f64
Levis <levislibra@hotmail.com>
2021-06-15 13:17:56 -0300

    Update

--- __openerp__.py
+++ __openerp__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'financiera_prestamos', 'financiera_pagos_360', 'financiera_sms', 'web_responsive'],
+    'depends': ['base', 'financiera_prestamos', 'financiera_pagos_360', 'financiera_sms', 'web_responsive', 'financiera_buro_rol_base'],
 
     # always loaded
     'data': [
